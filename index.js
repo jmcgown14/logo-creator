988d90c3f9d9aef602eaa1196219e35a27456b6a
Johnny McGown <jmac3292@gmail.com>
2023-03-09 17:18:49 -0700

    Updated main index, got validates working, created classes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const color = require("color")
+const color = require("color");
+// const Circle = require("./shapes")
 // const generateSVG = require()
 
+// Class for the SVG logo?
 // class LogoPrompt {
 //     constructor() {
 //         this.logoletters = null;
@@ -13,6 +15,7 @@ const color = require("color")
 // }
 
 // user prompts for custom logo
+// const askQuestions = () => {
 inquirer
     .prompt([
         {
@@ -20,8 +23,15 @@ inquirer
             name: 'logoLetters',
             message: 'Please enter up to 3 characters for you logo',
             // Validate maybe with a return for length of answer?
+            validate: (input) => {
+                if(input.length > 0 && input.length <= 3){
+                    return true;
+                }else{
+                    console.log('Please input atleast 3 characters');
+                    return false;
+            };
             // test parameter for this and how to do that?
-        },
+        }},
         {
             type: 'list',
             name: 'shapes',
@@ -33,28 +43,50 @@ inquirer
             name: 'textColor',
             message: 'Choose the color of your logo text by typing in  your color.',
             // another Validate with return?
-            // validate: (input) => {
-            //     const color= Color(input)
-            //     return =???
-            // }
-            // Need an NPM for color and how do you pull from it?
-        },
+            validate: (input) => {
+                if(color){
+                    return true;
+            }else{
+                console.log('Pick another color.');
+                return false;
+            }
+        }},
         {
             type: 'input',
             name: 'shapeColor',
             message: 'Choose the color of your logo shape by typing in your color.',
             // another Validate with return?
-            // validate: (input) => {
-            //     const color= Color(input)
-            //     return =???
-            // }
-            // Need an NPM for color and how do you pull from it?
-        },
+            validate: (input) => {
+                if(color){
+                    return true;
+            }else{
+                console.log('Pick another color.');
+                return false;
+            }
+        }},
 
     ])
+    .then((answers) => {
+        console.log(answers);})
+        // var shapes;
+        // if (answers.shapes === "circle") {
+        //     shapes = new Circle()
+        //     console.log(shapes)
+        // }
+        // if (answers.shapes === "square") {
+        //     shapes = new Circle()
+        //     console.log(shapes)
+        // }
+        // if (answers.shapes === "triangle") {
+        //     shapes = new Circle()
+        //     console.log(shapes)
+        // }
+        // shapes.setColor
 
+
+// askQuestions();
 // What function do I need to do generate te SVG file?
-
+// generateSVG
 
 
 //     ## Acceptance Criteria
